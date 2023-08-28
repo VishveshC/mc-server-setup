3af832b02780ea00a79d29173ecc3812aaa8f04e
OpenDoor <stepper1947@gmail.com>
2023-08-28 14:00:03 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,6 +10,19 @@ apt-get install -y screen
 sleep 0.5
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
 sleep 0.5
-screen -d -m http-server /cloudclusters -p 6969 -a 127.0.0.1
-curl -o rclone.zip https://downloads.rclone.org/rclone-current-windows-amd64.zip
-curl -o graalvm.zip https://oca.opensource.oracle.com/gds/GRAALVM_EE_JAVA17_22_3_1/graalvm-ee-java17-linux-amd64-22.3.1.tar.gz
+curl -o graalvm.tar.gz https://oca.opensource.oracle.com/gds/GRAALVM_EE_JAVA17_22_3_1/graalvm-ee-java17-linux-amd64-22.3.1.tar.gz
+sleep 0.5
+mkdir javahome
+sleep 0..5
+tar -C javahome -xvf graalvm.tar.gz
+sleep 0.5
+mkdir mc
+sleep 0.5
+curl -o mc/server.jar https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.14.22/0.11.2/server/jar
+sleep 0.5
+java -jar fabric-installer.jar server
+sleep 0.5
+
+
+
+****screen -d -m 
